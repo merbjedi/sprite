8f65bc932f374dc0100180eaa877624a58f4ba19
amikula <amikula@gmail.com>
2010-03-11 17:25:02 -0800

    Adding in require of rmagick

--- lib/sprite/image_reader.rb
+++ lib/sprite/image_reader.rb
@@ -1,8 +1,10 @@
 module Sprite
   class ImageReader
     def self.read(image_filename)
-      image = Magick::Image::read(image_filename).first
-      image
+      # avoid loading rmagick till the last possible moment
+      require 'rmagick'
+
+      Magick::Image::read(image_filename).first
     end
   end
 end
