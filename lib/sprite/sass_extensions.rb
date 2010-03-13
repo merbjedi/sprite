373d1c45d11aae9c959da7426bc7d763d0fab289
amikula <amikula@gmail.com>
2010-03-12 17:21:50 -0800

    More sass extensions for roll-your-own mixins
    sprite_x_offset, sprite_y_offset, sprite_image extensions.

--- lib/sprite/sass_extensions.rb
+++ lib/sprite/sass_extensions.rb
@@ -11,25 +11,36 @@ module Sprite
         end
       end
 
-      def sprite_width(group, image)
+      def sprite_width(*args)
+        sprite_attr(:width, *args)
+      end
+
+      def sprite_height(*args)
+        sprite_attr(:height, *args)
+      end
+
+      def sprite_x_offset(*args)
+        sprite_attr(:x, *args)
+      end
+
+      def sprite_y_offset(*args)
+        sprite_attr(:y, *args)
+      end
+
+      def sprite_image(group)
+        sprite_builder.image_path(group.value)
+      end
+
+    protected
+      def sprite_attr(attr, group, image)
         sprite = sprite_data(group, image)
         if sprite
-          "#{sprite[:width]}px"
+          "#{sprite[attr]}px"
         else
           ""
         end
       end
-      
-      def sprite_height(group, image)
-        sprite = sprite_data(group, image)
-        if sprite
-          "#{sprite[:height]}px"
-        else
-          ""
-        end
-      end
-      
-      protected
+
       def sprite_builder
         @__sprite_builder ||= Builder.from_config
       end
@@ -37,11 +48,11 @@ module Sprite
       def sprite_data(group, image)
         unless @__sprite_data
           sprite_data_path = sprite_builder.style_output_path
-          
+
           # read sprite data from yml
           @__sprite_data = File.open(sprite_data_path) { |yf| YAML::load( yf ) }
         end
-        
+
         group_data = @__sprite_data[group.to_s]
         if group_data
           return group_data[image.to_s]
@@ -49,7 +60,6 @@ module Sprite
           nil
         end
       end
-      
     end
   end
 end
